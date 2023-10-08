51bfbe24added6d31ee5c775e3ddf7e56d20ffe9
Talos <ismail.sahbane@epfl.ch>
2023-10-08 13:09:34 +0200

    add function signatures

--- implementations.py
+++ implementations.py
@@ -1,4 +1,21 @@
 import numpy as np
 
-# the functions to implement
-...
+# ==================== the functions to implement ====================
+
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gammma):
+    ...
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gammma):
+    ...
+
+def least_squares(y, tx):
+    ...
+
+def ridge_regression(y, tx, lambda_):
+    ...
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    ...
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    ...
